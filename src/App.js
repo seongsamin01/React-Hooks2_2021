9c5298270ea16224f233c12d7acde82bada30a53
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-30 11:20:24 +0900

    useReducer 이해하기 복습
    switch구문 대신 if else사용하기,
    근데 if else구문같은 경우에는 계쏙 if else를 써주고 ===를 써줘야되서 번거로움.
    
    dispatch function이 useReducer를 실행시킴. 그래서 dispatch랑 useReducer은 함께 실행됨.
    그래서 type, switch문도 사실 쓸 필요는 없음.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import React, { useReducer } from "react";
   const DECREMENT = "decrement";
 
   const reducer = (state, action) => {
-    switch (action.type) {
-      case INCREMENT:
-        return { count: state.count + 1 };
-      case DECREMENT:
-        return { count: state.count - 1 };
-      default:
-        throw new Error();  
+    if (action === INCREMENT) {
+      return { count: state.count + 1 };
+    } else if (action === DECREMENT) {
+      return { count: state.count - 1 };
     }
-  }
-
+    return { count: 99 };
+  };
+    
   function App() {
-    const [state, dispatch] = useReducer(reducer, { count: 0});
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
     return (
       <>
         <h1>{state.count}</h1>
-        <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-        <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+        <button onClick={() => dispatch (INCREMENT)}>Add</button>
+        <button onClick={() => dispatch (DECREMENT)}>Remove</button>
       </>  
     );
   }
