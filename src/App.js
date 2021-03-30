fd7694a07b1382e82467df94fe2af46f98cadadb
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-30 12:50:05 +0900

    To do list #항목삭제기능
    Filter는 array를 수정하는 것이 아니라, 새로운 array를 만드는 것이다.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,57 @@
 import React, { useReducer, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-  const initialState = {
-    toDos: []
-  };  
+    const initialState = {
+      toDos: []
+    };  
 
-  const ADD = "add";
+    const ADD = "add";
+    const DEL = "del";
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case ADD:
-        return {toDos: [ ...state.toDos, { text: action.payload }]};
-      default:
-        return; 
-    }
-  };
-    
-  function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [newToDo, setNewToDo] = useState("");
-    const onSubmit = e => {
-      e.preventDefault();
-      dispatch({ type: ADD, payload: newToDo });
+    const reducer = (state, action) => {
+      switch (action.type) {
+        case ADD:
+          return {toDos: [ ...state.toDos, { text: action.payload, id: uuidv4() }]};
+        case DEL:
+          return {toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
+          };
+        default:
+          return; 
+      }
     };
-    const onChange = e => {
-      const {
-        target: { value }
-      } = e;
-      setNewToDo(value);
-    };
-    return (
-      <>
-        <h1>Add to do</h1>
-        <form onSubmit={onSubmit}>
-          <input 
-            value={newToDo}
-            type="text"
-            placeholder="Add to your to do list..."
-            onChange={onChange}
-          />  
-        </form>
-
-        <ul>
-          <h2>MY To Do List</h2>
-          {state.toDos.map((toDo, index) => ( 
-            <li key={index}>{toDo.text}</li>
+      
+    function App() {
+      const [state, dispatch] = useReducer(reducer, initialState);
+      const [newToDo, setNewToDo] = useState("");
+      const onSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: ADD, payload: newToDo });
+        setNewToDo("");
+      };
+      const onChange = e => {
+        const {
+          target: { value }
+        } = e;
+        setNewToDo(value);
+      };
+      return (
+        <>
+          <h1>Add to do</h1>
+          <form onSubmit={onSubmit}>
+            <input 
+              value={newToDo}
+              type="text"
+              placeholder="Add to your to do list..."
+              onChange={onChange}
+            />  
+          </form>
+          <ul>
+            <h2>MY To Do List</h2>
+            {state.toDos.map(toDo => ( 
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>👍</button>
+              </li>
             ))}
         </ul>
       </>  
