5e97eae4a09d4f2d0a5b24eabea510a21862947b
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-03-30 14:22:47 +0900

    To do lisit #Complete한경우
    대문자로 선언해준 경우엔,
    import 다 해줘야 되는 구나.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React, { useReducer, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import reducer, { initialState, ADD, DEL, COMPLETE, UNCOMPLETE } from "./reducer";
 
-    const initialState = {
-      toDos: []
-    };  
-
-    const ADD = "add";
-    const DEL = "del";
-
-    const reducer = (state, action) => {
-      switch (action.type) {
-        case ADD:
-          return {toDos: [ ...state.toDos, { text: action.payload, id: uuidv4() }]};
-        case DEL:
-          return {toDos: state.toDos.filter(toDo => toDo.id !== action.payload)
-          };
-        default:
-          return; 
-      }
-    };
       
     function App() {
       const [state, dispatch] = useReducer(reducer, initialState);
@@ -50,9 +32,25 @@ import { v4 as uuidv4 } from 'uuid';
             {state.toDos.map(toDo => ( 
               <li key={toDo.id}>
                 <span>{toDo.text}</span>
-                <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>👍</button>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>👍</span>
               </li>
             ))}
+          </ul>
+          <ul>
+            {state.completed.length !== 0 && (
+              <>
+            <h2>Completed</h2>
+            {state.completed.map(toDo => ( 
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  👍
+                </span>
+                <span onClick={() => dispatch({ type: UNCOMPLETE, payload: toDo.id })}>
+                  👎
+                </span>
+              </li>
+          ))}</>)}
         </ul>
       </>  
     );
